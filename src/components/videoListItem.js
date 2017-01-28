02708a6f723715264b73d3dd72b75c33b8a077fd
Ravi Yadav <raviyadav1806@gmail.com>
2017-01-28 11:17:04 +0530

    Search results out with Video Details next up

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -6,8 +6,24 @@ import React from 'react';
 // we refer to those props in here as props.key or props.video
 //so here we can get it as const video = props.video OR we 
 //we can just use es6 new feature and pass in video as props cos we only need that and key prop is unused.
-const VideoListItem = (props) => {
-  return <li>Video</li>
+//const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  const imgUrl = video.snippet.thumbnails.default.url ;
+  const title = video.snippet.title;
+  return (
+		<li className = "list-group-item">
+  			<div className = "video-list media">
+  	           	<div className = "media-left">
+  	           		<img className = "media-object" src={imgUrl}/>
+  	           	</div>
+
+				<div className = "media-body">
+					<div className = "media-heading">{title}</div>
+				</div>
+			</div>
+		</li>
+		);
+
 };
 
 export default VideoListItem;
